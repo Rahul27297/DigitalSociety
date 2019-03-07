73311141ca6a45d530586c04afdca57112408930
Rahul27297 <rahul.chaudhari27297@gmail.com>
2019-03-07 21:29:55 +0530

    Profile fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -94,6 +94,7 @@ export class MyApp {
   checkLogin() {
     this.storage.get('Info').then((val) => {
       if (val == null) {
+        this.splashScreen.hide();
         this.rootPage = LoginPage;
       } else {
         this.storage.get("societyId").then((val) => {
@@ -104,6 +105,7 @@ export class MyApp {
             this.societyInfo = Object.getOwnPropertyDescriptor(societysnapshot.val(),tempKey).value;
             console.log(this.societyInfo);
             this.societyName = this.societyInfo.display_name;
+            this.splashScreen.hide();
             this.nav.push(HomePage, {
               societyInfo: this.societyInfo,
               societyId: this.societyId
@@ -128,7 +130,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      
     });
   }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
+import * as firebase from 'firebase';
 
 /**
  * Generated class for the ProfilePage page.
@@ -21,19 +23,10 @@ export class ProfilePage {
   private clientFlat: any;
   private societyName: any;
   private societyAddress: any;
+  private loader: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
-    
-    this.storage.get('Info').then( (res) =>{ 
-
-      this.clientName = res.name;
-      this.clientEmail = res.email;
-      this.clientPhone = res.phone;
-      //this.clientAddress = res.address2;
-      this.clientFlat = res.address1;
-      this.societyName = this.navParams.get('societyName');
-      this.societyAddress = this.navParams.get('address');
-    });
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage, private loadingCtrl: LoadingController) {
+  
   }
 
   ionViewDidEnter(){
@@ -41,7 +34,30 @@ export class ProfilePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
-    
+    this.loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    this.loader.present();
+    this.storage.get('Info').then( (res) =>{ 
+
+      this.clientName = res.name;
+      this.clientEmail = res.email;
+      this.clientPhone = res.phone;
+      //this.clientAddress = res.address2;
+      this.clientFlat = res.address1;
+      
+      this.storage.get('societyId').then((res) => {
+        let societyId = res;
+        firebase.database().ref('societies').orderByChild('society_id').equalTo("" + societyId).on('value', (societysnapshot) => {
+          console.log("Information Stored");
+            let tempKey = Object.keys(societysnapshot.val())[0];
+            let societyInfo = Object.getOwnPropertyDescriptor(societysnapshot.val(),tempKey).value;
+            this.societyName = societyInfo.display_name;
+            this.societyAddress = societyInfo.address;
+        });
+      });
+    });
+    this.loader.dismiss();  
   }
 
 }
